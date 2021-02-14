9519676f28e409112dae4aa7e05cf1569eebaa07
Ilie Alexandru <45337876+Liniuta@users.noreply.github.com>
2021-02-14 21:59:33 +0200

    bug fix
    adding a url to working urls wasn't working and variable "Valid" wasn't found.

--- iptv-checker.py
+++ iptv-checker.py
@@ -27,7 +27,9 @@ try:
     with open(txt_file) as f:
         text = f.read()
         urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+        channels = text.split(',')[1]
         print("Number of streams loaded:", len(urls))
+        print("Channles loaded:", channels)
         working_urls = []
         for url in urls:
             try:
@@ -35,16 +37,15 @@ try:
                 sc = r.status_code
                 if sc == 200:
                     valid = 'Valid'
+                    working_urls.append(url)
                 else:
-                    #valid = "Not valid"
-                    pass
+                    valid = "Not valid"
                 now = datetime.now()
                 date_and_time = now.strftime("%d/%m/%Y %H:%M:%S")
                 print(f'URL: {url}')
                 #print(f'Status code: {sc}') 
                 print(f'{valid}')
                 print(f"Checked at {date_and_time}.")
-                working_urls.append(url)
                 time.sleep(int(timeout))
             except (requests.exceptions.RequestException, requests.exceptions.Timeout, requests.exceptions.ConnectionError) as e:
                 now = datetime.now()
@@ -55,6 +56,7 @@ try:
                 time.sleep(int(timeout))
     os.rename("playlist_temp.txt", m3u_file)
     time.sleep(2)
+    print(working_urls)
     print('Generating checked playlist...')
     with open('playlist-checked.m3u', 'w') as file:
         file.write('#EXTM3U\n') 
